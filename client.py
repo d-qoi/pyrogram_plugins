56ac84d3d83a414f51aae3482a50e07e8c11c21c
Alex Hirschfeld <alex@d-qoi.com>
2019-10-31 22:21:52 -0500

    Now with working client.py and .gitignore

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+import logging
+
 from pyrogram import Client
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 app = Client("CustClient", config_file="config.ini")
+app.run()
